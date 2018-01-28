608ebbb10203e9c9ded6d03169814e673ad7869c
nikhilnagaraju <nikhilnagaraju96@gmail.com>
2018-01-28 17:36:17 +0530

    added array endpoint

--- api_demo/js/main.js
+++ api_demo/js/main.js
@@ -3,10 +3,17 @@ var container = $('div.container');
 $(document).ready(function() {
   $('#GSD').click(function () {
 
+    var radioValue = $("input[name='options']:checked").val();
+    if (radioValue==="option1"){
+      var endurl = "https://random-kan-fact.herokuapp.com/random";
+    }else {
+        var endurl = "https://random-kan-fact.herokuapp.com/facts";
+    }
+
     $.ajax({
       // url: "http://localhost:8000/random",
       type: 'GET',
-      url: "https://random-kan-fact.herokuapp.com/random",
+      url: endurl,
       dataType: 'json',
       success: function (data) {
         $('#resjson').text(JSON.stringify( data, null, '  '));
